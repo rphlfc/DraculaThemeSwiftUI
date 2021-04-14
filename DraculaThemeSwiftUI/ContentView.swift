7b761424d82ce5df3675d00262ebcc88f8bb023d
Raphael Cerqueira <raphaelfc@gmail.com>
2021-04-14 20:43:51 -0300

    Primeiro commit

--- DraculaThemeSwiftUI/ContentView.swift
+++ DraculaThemeSwiftUI/ContentView.swift
@@ -5,12 +5,40 @@
 //  Created by Raphael Cerqueira on 14/04/21.
 //
 
+import Dracula
 import SwiftUI
 
 struct ContentView: View {
+    let colors = [
+        Color.draculaCurrent,
+        Color.draculaForeground,
+        Color.draculaComment,
+        Color.draculaCyan,
+        Color.draculaGreen,
+        Color.draculaOrange,
+        Color.draculaPink,
+        Color.draculaPurple,
+        Color.draculaRed,
+        Color.draculaYellow
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack(alignment: .leading) {
+            Color.draculaBackground
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading) {
+                ForEach(0..<colors.count) { index in
+                    Text("Dracula Theme")
+                        .font(.system(size: CGFloat(29 - index)))
+                        .foregroundColor(colors[index])
+                        .padding()
+                }
+                
+                Spacer()
+            }
+            .background(Color.draculaBackground)
+        }
     }
 }
 
---
+++ README.md
@@ -0,0 +1,26 @@
+# Dracula para SwiftUI
+Utilização do tema (Dracula)[https://draculatheme.com] para SwiftUI.
+
+## Screenshots
+<p align="center">
+    <img src="https://user-images.githubusercontent.com/16376748/114793909-cce6d300-9d61-11eb-9983-02acf20b0aee.png" width="200"> &nbsp;
+</p>
+
+## Requisitos
+- iOS 14
+- SwiftUI
+- Xcode 12
+
+## Meta
+- Github: [https://github.com/rphlfc](https://github.com/rphlfc)
+- Instagram: [https://www.instagram.com/rphlfc.dev](https://www.instagram.com/rphlfc.dev)
+- LinkedIn: [http://www.linkedin.com/in/rphlfc](http://www.linkedin.com/in/rphlfc)
+- Twitter: [https://twitter.com/rphlfc](https://twitter.com/rphlfc)
+- Website: [https://rphlfc.github.io](https://rphlfc.github.io)
+- Youtube: [https://www.youtube.com/channel/UC7UXnCqiqsV4R9lCwKh-rwg](https://www.youtube.com/channel/UC7UXnCqiqsV4R9lCwKh-rwg)
+
+
+
+
+
+
